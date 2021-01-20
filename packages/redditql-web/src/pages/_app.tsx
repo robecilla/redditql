479c56e6360668c6bd4e454fa767c5338a40190f
robecilla <roberto.vecilla@gmail.com>
2021-01-20 18:28:54 +0100

    do not use theme or color providers from chakra

--- packages/redditql-web/src/pages/_app.tsx
+++ packages/redditql-web/src/pages/_app.tsx
@@ -1,17 +1,9 @@
-import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-
-import theme from "../theme";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function redditql({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
+    <ChakraProvider resetCSS>
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
